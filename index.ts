67f979b86a06e98639ba8123ca5e7caf4c0cf10d
max.liu <max.liu@ihr360.com>
2024-10-25 11:22:22 +0800

    feat: add some infomation

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export { default as ApiManager } from "./entry/reducer";
-// export { default as ApiManagerRedux } from "./entry/redux";
+export { default as ApiManagerRedux } from "./entry/redux";
 export { setRequestConfig } from "./configs/sdk";
